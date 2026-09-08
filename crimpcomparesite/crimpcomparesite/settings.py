97c7fab02b1fc0700eaef88a9a2199d5d85523a4
QuichenBeans <alexhirsty6@gmail.com>
2026-09-08 16:12:39 +0100

    turned off debug

--- crimpcomparesite/crimpcomparesite/settings.py
+++ crimpcomparesite/crimpcomparesite/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '165.232.97.22',
